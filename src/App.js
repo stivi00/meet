02cb8722ab420fec1a935658072101e7bef1d783
stivi00 <stivisth@gmail.com>
2023-10-28 13:49:58 +0200

    more cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return <div className='App'></div>;
+    return (
+        <div className='App'>
+            <h1>Hello fixed world</h1>
+        </div>
+    );
 }
 
 export default App;

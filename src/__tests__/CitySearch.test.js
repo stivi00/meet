67e92b7dcc788e102178388490c402d14f19a15b
stivi00 <stivisth@gmail.com>
2023-10-28 21:59:03 +0200

    city search user interaction tests added

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -2,6 +2,7 @@
 /* eslint-disable testing-library/render-result-naming-convention */
 
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import CitySearch from '../components/CitySearch';
 
 describe('<CitySearch /> component', () => {
@@ -10,5 +11,21 @@ describe('<CitySearch /> component', () => {
         const cityTextBox = CitySearchComponent.queryByRole('textbox');
         expect(cityTextBox).toBeInTheDocument();
         expect(cityTextBox).toHaveClass('city');
+    });
+
+    test('suggestions list is hidden by default', () => {
+        const CitySearchComponent = render(<CitySearch />);
+        const suggestionList = CitySearchComponent.queryByRole('list');
+        expect(suggestionList).not.toBeInTheDocument();
+    });
+
+    test('renders a list of suggestions when city textbox gains focus', async () => {
+        const CitySearchComponent = render(<CitySearch />);
+        const user = userEvent.setup();
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
+        await user.click(cityTextBox);
+        const suggestionList = CitySearchComponent.queryByRole('list');
+        expect(suggestionList).toBeInTheDocument();
+        expect(suggestionList).toHaveClass('suggestions');
     });
 });

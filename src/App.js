844ea2a8d4cedf30f3109101fcf86ae130adb8ee
stivi00 <stivisth@gmail.com>
2023-10-28 19:24:48 +0200

    removed tests for citysearch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import './App.css';
 
 const App = () => {
     return (
         <div className='App'>
-            <CitySearch />
             <EventList />
         </div>
     );
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -12,8 +12,4 @@ describe('<App /> component', () => {
     test('renders list of events', () => {
         expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
     });
-
-    test('render CitySearch', () => {
-        expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
-    });
 });

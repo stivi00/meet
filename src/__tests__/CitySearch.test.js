d27afde0d5662f48a9699ce7c0ced67a9a6d3d6d
stivi00 <stivisth@gmail.com>
2023-10-28 23:58:17 +0200

    coverage and citySearch test added

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,3 +1,5 @@
+/* eslint-disable testing-library/no-render-in-setup */
+/* eslint-disable no-undef */
 /* eslint-disable testing-library/prefer-screen-queries */
 /* eslint-disable testing-library/render-result-naming-convention */
 
@@ -5,27 +7,94 @@ import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import CitySearch from '../components/CitySearch';
 
-describe('<CitySearch /> component', () => {
+import { extractLocations, getEvents } from '../api';
+
+describe('<CitySearch/> component', () => {
+    let CitySearchComponent;
+    beforeEach(() => {
+        CitySearchComponent = render(<CitySearch allLocations={[]} />);
+    });
+
+    // # TEST 1
+
     test('renders text input', () => {
-        const CitySearchComponent = render(<CitySearch />);
         const cityTextBox = CitySearchComponent.queryByRole('textbox');
+
         expect(cityTextBox).toBeInTheDocument();
         expect(cityTextBox).toHaveClass('city');
     });
 
+    // #TEST 2'
+
     test('suggestions list is hidden by default', () => {
-        const CitySearchComponent = render(<CitySearch />);
         const suggestionList = CitySearchComponent.queryByRole('list');
         expect(suggestionList).not.toBeInTheDocument();
     });
 
+    // #TEST 3
+
     test('renders a list of suggestions when city textbox gains focus', async () => {
-        const CitySearchComponent = render(<CitySearch />);
         const user = userEvent.setup();
         const cityTextBox = CitySearchComponent.queryByRole('textbox');
         await user.click(cityTextBox);
         const suggestionList = CitySearchComponent.queryByRole('list');
         expect(suggestionList).toBeInTheDocument();
         expect(suggestionList).toHaveClass('suggestions');
+    });
+
+    // #TEST4
+
+    test('updates list of suggestions correctly when user types in city textbox', async () => {
+        const user = userEvent.setup();
+        const allEvents = await getEvents();
+        const allLocations = extractLocations(allEvents);
+        CitySearchComponent.rerender(
+            <CitySearch allLocations={allLocations} />
+        );
+
+        // user types "Berlin" in city textbox
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
+        await user.type(cityTextBox, 'Berlin');
+
+        // filter allLocations to locations matching "Berlin"
+        const suggestions = allLocations
+            ? allLocations.filter((location) => {
+                  return (
+                      location
+                          .toUpperCase()
+                          .indexOf(cityTextBox.value.toUpperCase()) > -1
+                  );
+              })
+            : [];
+
+        // get all <li> elements inside the suggestion list
+        const suggestionListItems =
+            CitySearchComponent.queryAllByRole('listitem');
+        expect(suggestionListItems).toHaveLength(suggestions.length + 1);
+        for (let i = 0; i < suggestions.length; i++) {
+            expect(suggestionListItems[i].textContent).toBe(suggestions[i]);
+        }
+    });
+
+    // #TEST 5
+
+    test('renders the suggestion text in the textbox upon clicking on the suggestion', async () => {
+        const user = userEvent.setup();
+        const allEvents = await getEvents();
+        const allLocations = extractLocations(allEvents);
+        CitySearchComponent.rerender(
+            <CitySearch allLocations={allLocations} setCurrentCity={() => {}} />
+        );
+
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
+        await user.type(cityTextBox, 'Berlin');
+
+        // the suggestion's textContent look like this: "Berlin, Germany"
+        const BerlinGermanySuggestion =
+            CitySearchComponent.queryAllByRole('listitem')[0];
+
+        await user.click(BerlinGermanySuggestion);
+
+        expect(cityTextBox).toHaveValue(BerlinGermanySuggestion.textContent);
     });
 });

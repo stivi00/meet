a983eb56dffb90e506e732f9e75d048b991956a5
stivi00 <stivisth@gmail.com>
2023-11-01 15:34:12 +0100

    added googles html for verification

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,6 +4,23 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
+const { ResizeObserver } = window;
+
+beforeEach(() => {
+    //@ts-ignore
+    delete window.ResizeObserver;
+    window.ResizeObserver = jest.fn().mockImplementation(() => ({
+        observe: jest.fn(),
+        unobserve: jest.fn(),
+        disconnect: jest.fn(),
+    }));
+});
+
+afterEach(() => {
+    window.ResizeObserver = ResizeObserver;
+    jest.restoreAllMocks();
+});
+
 // Here, add portions of the warning messages you want to intentionally prevent from appearing
 const MESSAGES_TO_IGNORE = [
     'When testing, code that causes React state updates should be wrapped into act(...):',
